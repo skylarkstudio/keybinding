490a3cfa1f2a0ba72b50b2f90b0e363770fd1a56
Joshua Granick <jgranick@users.noreply.github.com>
2017-04-10 14:59:17 -0700

    Update namespace

--- com/eclecticdesignstudio/control/KeyBindingData.hx
+++ com/eclecticdesignstudio/control/KeyBindingData.hx
@@ -1,26 +1,4 @@
-﻿/**
- * @author Joshua Granick
- */
+﻿package com.eclecticdesignstudio.control;
 
 
-package com.eclecticdesignstudio.control;
-
-
-class KeyBindingData {
-	
-	
-	public var key:Int;
-	public var handler:Dynamic;
-	public var repeat:Bool;
-	
-	
-	public function new (key:Int, handler:Dynamic, repeat:Bool = false) {
-		
-		this.key = key;
-		this.handler = handler;
-		this.repeat = repeat;
-		
-	}
-	
-	
-}
+typedef KeyBindingData = skylark.control.KeyBindingData;
---
+++ skylark/control/KeyBinding.hx
@@ -0,0 +1,155 @@
+﻿/**
+ * Manages keyboard controls
+ * @author Joshua Granick
+ */
+
+
+package skylark.control;
+	
+
+import flash.display.Stage;
+import flash.events.KeyboardEvent;
+import flash.ui.Keyboard;
+import flash.Lib;
+
+#if haxe3
+import haxe.ds.IntMap;
+#else
+typedef IntMap<T> = IntHash<T>;
+#end
+
+
+class KeyBinding {
+	
+	
+	private static var initialized:Bool;
+	private static var keyPressed = new IntMap <Bool> ();
+	private static var pressBindings = new IntMap <KeyBindingData> ();
+	private static var releaseBindings = new IntMap <KeyBindingData> ();
+	
+	
+	/**
+	 * Adds a new handler for when the specified key is pressed. Each key can only have one onPress handler
+	 * @param	key		The key to listen for. Strings (like "k") and objects (like Keyboard.UP) are supported
+	 * @param	handler		The function to call when the key has been pressed
+	 * @param	handlerParams		(Optional) Parameters to apply to the handler function
+	 * @param	repeat		Whether the handler should be repeatedly called as the key remains pressed
+	 */
+	public static function addOnPress (key:Dynamic, handler:Dynamic, repeat:Bool = false):Void {
+		
+		initialize ();
+		
+		if (Std.is (key, String)) {
+			
+			key = getKeyCode (key);
+			
+		}
+		
+		pressBindings.set (key, new KeyBindingData (key, handler, repeat));
+		
+	}
+	
+	
+	/**
+	 * Adds a new handler for when the specified key is released. Each key can only have one onRelease handler
+	 * @param	key		The key to listen for. Strings (like "k") and objects (like Keyboard.UP) are supported
+	 * @param	handler		The function to call when the key has been released
+	 * @param	handlerParams		(Optional) Parameters to apply to the handler function
+	 */
+	public static function addOnRelease (key:Dynamic, handler:Dynamic):Void {
+		
+		initialize ();
+		
+		if (Std.is (key, String)) {
+			
+			key = getKeyCode (key);
+			
+		}
+		
+		releaseBindings.set (key, new KeyBindingData (key, handler));
+		
+	}
+	
+	
+	public static function getKeyCode (key:String):Int {
+		
+		return key.toUpperCase ().charCodeAt (0);
+		
+	}
+	
+	
+	/**
+	 * Initializes the KeyBinding class
+	 * @param	stage
+	 */
+	private static function initialize ():Void {
+		
+		if (!initialized) {
+			
+			Lib.current.stage.addEventListener (KeyboardEvent.KEY_DOWN, stage_onKeyDown);
+			Lib.current.stage.addEventListener (KeyboardEvent.KEY_UP, stage_onKeyUp);
+			
+			initialized = true;
+			
+		}
+		
+	}
+	
+	
+	/**
+	 * Removes bindings for the specified key
+	 * @param	key		The key to remove bindings for. Strings (like "k") and objects (like Keyboard.UP) are supported
+	 */
+	public static function remove (key:Int):Void {
+		
+		pressBindings.remove (key);
+		releaseBindings.remove (key);
+		
+	}
+	
+	
+	
+	
+	// Event Handlers
+	
+	
+	
+	
+	private static function stage_onKeyDown (event:KeyboardEvent):Void {
+		
+		var key = event.keyCode;
+		
+		if (pressBindings.exists (key)) {
+			
+			var data = pressBindings.get (key);
+			
+			if (data.repeat || !keyPressed.exists (key)) {
+				
+				Reflect.callMethod (data.handler, data.handler, []);
+				
+			}
+			
+			keyPressed.set (key, true);
+			
+		}
+		
+	}
+	
+	
+	private static function stage_onKeyUp (event:KeyboardEvent):Void {
+		
+		var key = event.keyCode;
+		keyPressed.remove (key);
+		
+		if (releaseBindings.exists (key)) {
+			
+			var data = releaseBindings.get (key);
+			
+			Reflect.callMethod (data.handler, data.handler, []);
+			
+		}
+		
+	}
+	
+	
+}
